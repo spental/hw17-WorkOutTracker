f266c6bfbe54ae6e216d40a6ef7b00c681304d0d
Kyle Talbott <codekingkyle@gmail.com>
2021-04-20 08:31:47 -0400

    asf

--- server.js
+++ server.js
@@ -1,27 +1,79 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const path = require("path");
 const logger = require("morgan");
+const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3000;
-// express app//
+const PORT = process.env.PORT || 3030;
+
+const db = require("./models");
 const app = express();
+
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// set our mongoose connection to  the mongo atlas uri or local host//
+
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-  useUnifiedTopology: true
+    useNewUrlParser: true,
+
 });
 
-// require our html and api routes//
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
-//listen to the port
-app.listen(PORT,()=>{
-    console.log(`listen to the port ${PORT}`);
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/exercise.html"));
+});
+
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/stats.html"));
+});
+
+app.get("/api/workouts", (req, res) => {
+    db.Workout.find({}, null, { sort: { day: 1 } })
+        .populate("exercises")
+        .then((dbWorkout) => {
+            res.json(dbWorkout);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+});
+
+app.put("/api/workouts/:id", (req, res) => {
+    var workoutID = req.params.id;
+    db.Exercise.create(req.body)
+        .then(({ _id }) =>
+            db.Workout.findOneAndUpdate({ _id: workoutID }, { $push: { exercises: _id } }, { new: true })
+        )
+        .then((dbWorkout) => {
+            res.json(dbWorkout);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+});
+
+app.post("/api/workouts", (req, res) => {
+    db.Workout.create(req.body)
+        .then((dbWorkout) => {
+            res.json(dbWorkout);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+});
+
+app.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({}, null, { sort: { day: 1 } })
+        .populate("exercises")
+        .then((dbWorkout) => {
+            res.json(dbWorkout);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+});
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
 });
