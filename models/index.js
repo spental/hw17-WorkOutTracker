8a3be187a2951173326632ca0b071fa23b4e4219
Mr. Spencer <codekingkyle@gmail.com>
2021-04-21 01:15:09 -0400

    mongo fix finally connects

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
-module.exports = {
-    workout: require("./workout")
-};
 
-//this file requires the workout schema we created in workout.js and we export this file to our api route//
+module.exports = {
+	Workout: require("./Workout"),
+	Exercise: require("./Exercise"),
+};
--- models/workout.js
+++ models/workout.js
@@ -1,58 +1,18 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-// creating the workout schema for adding exercies on a workout day. some fields have validation to require input//
 const WorkoutSchema = new Schema({
-    day: {
-        type: Date,
-        default: Date.now
-    },
-    exercises: [{
-        type: {
-            type: String,
-            trim: true,
-            required: "Enter the type of exercise you wish to add"
-        },
-        name:{
-            type: String,
-            trim: true,
-            required: "Enter the name of the exercise you wish to add"
-        },
-        duration: {
-            type: Number,
-            trim: true,
-            required: "Enter the duration of this exercise"
-        },
-        weight: {
-            trim: true,
-            type: Number,
-        },
-        reps: {
-            trim: true,
-            type: Number,
-        },
-        sets: {
-            trim: true,
-            type: Number,
-        },
-        distance: {
-            trim: true,
-            type: Number,
-        }
-    }]
-}, {
-    toJSON: {
-        // include any virtual properties when data is requested
-        virtuals: true
-    }
-});
-// adds a dynamically-created property to schema
-WorkoutSchema.virtual("totalDuration").get(function () {
-    // "reduce" array of exercises down to just the sum of their durations
-    return this.exercises.reduce((total, exercise) => {
-        return total + exercise.duration;
-    }, 0);
+	
+	day: {
+		type: Date,
+	},	
+	exercises: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: "Exercise",
+		},
+	],
 });
 
-//exporting the workout schema//
-const workout = mongoose.model("workout", WorkoutSchema);
-module.exports = workout;
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
